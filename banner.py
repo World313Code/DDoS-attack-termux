83e4d58bd0eeea60fe4587586f61bdaf9d19de78
World313Code <78675117+World313Code@users.noreply.github.com>
2021-02-07 16:44:19 -0300

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,42 @@
+black = '\u001b[30;1m'
+red = '\u001b[31;1m'
+green = '\u001b[32;1m'
+yellow = '\u001b[33;1m'
+blue = '\u001b[34;1m'
+pink= '\u001b[35;1m'
+cian= '\u001b[36;1m'
+white= '\u001b[37;1m'
+Reset = '\u001b[0m'
+
+def banner():
+    print(f'''{red}         ,o;c:;,,;;;;;,:;````.,```,,;;;:;,;:;,.`;;;;;,`;l:
+        `MMMWXXNNNNWWNWWMMMMMWWMWWWWMMMMMMMMMMWWNNNXNXWMMMk
+         `KMWx`         ...          ......         .oWMMK.
+          `XMMMXl                                 .dWMMMO
+            xMNxXNd.                            .xWO0MMk
+             cNX`oNWd                         ;KWk,xMMo
+              .KNo.cNWk.                    cXM0..0MN;
+                OMK. lXWd.                :NMk, :WMX`
+                 dMX.  :KWk,           .dNWx.  dMMk.
+                  :WN`   lNMO`       .oWMx.   dMWc
+                   cMWl    lXMk,   .dWNo.   .KMX.
+                    ,XMk     lXM0:dWXo.    ;WMk.
+                     .KMK.     lMMMk      oWWc
+                       oWX,  `dNkc0MO`  .OMX.
+                        cWWc0Wx.   ;KMklNMx.
+                         xMMW.       ;WMMk
+                      ;OWNloW0.     `KWokWNl
+                    .0MWo   ,WMd   oWW`  .xWNc
+                    kkl.     `XMO.OMW:     .c0X
+                               xMMMO.           
+                              .xMMMd
+                    `NKkOkOx.,NMWd0MdxOK0OkddKd
+                      `:NM0.lWMN.  kWxKMMNc;`.
+                        .0MXMMX.    OMMMO`
+              ;d0K0O.     dMMMo    ,WMMMc     .o;,.
+             OMMM.      ,WOOONM0   xMMMcoW0;    .:kMMd
+             cMM:.  .`XMX.  .XM; OMMW`  .OMX:.  .`NMW.
+              .l0WMWXOo:     ;WMXMMN,     .dNMWWMW0dc
+                 ...          .NMMX.          ...
+                               ,M0
+                                l.''')
